f8d33b18c4ca4774acf3bceacf49217783ed455a
Valerii Solodkyi <vosolodkiy@gmail.com>
2025-09-22 14:08:19 +0300

    add task solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,81 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
-import { GoodsList } from './GoodsList';
+import GoodsList from './GoodsList';
+import { Good } from './types/Good';
+
+import * as goodsAPI from './api/goods';
 
 // import { getAll, get5First, getRed } from './api/goods';
 // or
 // import * as goodsAPI from './api/goods';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <h1>Dynamic list of Goods</h1>
+export const App: React.FC = () => {
+  const [goods, setGoods] = useState<Good[]>([]);
+  const [messageError, setMessageError] = useState<null | string>(null);
 
-    <button type="button" data-cy="all-button">
-      Load all goods
-    </button>
+  return (
+    <div className="App">
+      <h1>Dynamic list of Goods</h1>
 
-    <button type="button" data-cy="first-five-button">
-      Load 5 first goods
-    </button>
+      <button
+        type="button"
+        data-cy="all-button"
+        onClick={async () => {
+          try {
+            const data = await goodsAPI.getAll();
 
-    <button type="button" data-cy="red-button">
-      Load red goods
-    </button>
+            setGoods(data);
+            setMessageError(null);
+          } catch (err) {
+            setMessageError('Failed to load goods. Please try again.');
+            setGoods([]);
+          }
+        }}
+      >
+        Load all goods
+      </button>
 
-    <GoodsList goods={[]} />
-  </div>
-);
+      <button
+        type="button"
+        data-cy="first-five-button"
+        onClick={async () => {
+          try {
+            const data = await goodsAPI.get5First();
+
+            setGoods(data);
+            setMessageError(null);
+          } catch (err) {
+            setMessageError('Failed to load goods. Please try again.');
+            setGoods([]);
+          }
+        }}
+      >
+        Load 5 first goods
+      </button>
+
+      <button
+        type="button"
+        data-cy="red-button"
+        onClick={async () => {
+          try {
+            const data = await goodsAPI.getRedGoods();
+
+            setGoods(data);
+            setMessageError(null);
+          } catch (err) {
+            setMessageError('Failed to load goods. Please try again.');
+            setGoods([]);
+          }
+        }}
+      >
+        Load red goods
+      </button>
+
+      {messageError && (
+        <div style={{ color: 'red', marginTop: '10px' }}>{messageError}</div>
+      )}
+
+      <GoodsList goods={goods} />
+    </div>
+  );
+};
